23d1912cf2304197e69700bb3d79d2529a2406b1
ashoordoushow <ashoor12323@gmail.com>
2024-10-29 19:35:45 -0500

    Add create action

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -3,4 +3,15 @@ class ExercisesController < ApplicationController
     @exercises = Exercise.all
     render :index
   end
+
+  def create
+    @exercise = Exercise.create(
+      workout_id: params[:workout_id],
+      name: params[:name],
+      reps: params[:reps],
+      sets: params[:sets],
+      image_url: params[:image_url]
+    )
+    render :show
+  end
 end
--- test/controllers/exercises_controller_test.rb
+++ test/controllers/exercises_controller_test.rb
@@ -8,4 +8,11 @@ class ExercisesControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Exercise.count, data.length
   end
+
+  test "create" do
+    assert_difference "Exercise.count", 1 do
+      post "/exercises.json", params: { workout_id: 1 name: "bench", reps: 10, sets: "3 sets of 10", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgy5RWhaieyrevjvS6ErgThrTBiYQL44cfTA&s"}
+      assert_response 200
+    end
+  end
 end
